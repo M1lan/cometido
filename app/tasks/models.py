41e093aff12abd8db155231f7c2ad87421e49e1f
Patrick Oberherr <patrick.oberherr@gmail.com>
2016-02-02 13:23:37 +0100

    Move CRUD to base class - adapt tasks and usermodel

--- app/tasks/models.py
+++ app/tasks/models.py
@@ -1,55 +1,58 @@
+import datetime
+
 from marshmallow_jsonapi import Schema, fields
 from marshmallow import validate
 
 from app.users.models import UsersSchema
-from app.db import db
-
-
-class CRUD():
-
-    def add(self, resource):
-        db.session.add(resource)
-        return db.session.commit()
-
-    def update(self):
-        return db.session.commit()
-
-    def delete(self, resource):
-        db.session.delete(resource)
-        return db.session.commit()
+from app.basemodels import db, CRUD
 
 
 class Tasks(db.Model, CRUD):
     __tablename__ = 'tasks'
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(255), unique=True, nullable=False)
+    title = db.Column(db.String(255), nullable=False)
     description = db.Column(db.String(250), nullable=True)
     due_datetime = db.Column(db.DateTime, server_default=db.func.current_timestamp(), nullable=True)
     # TODO: Find a nice way to create and migrate enums!
     complexity = db.Column(db.Integer, nullable=False)
     urgency = db.Column(db.Integer, nullable=False)
     importance = db.Column(db.Integer, nullable=False)
-    #user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     creation_time = db.Column(db.DateTime, server_default=db.func.current_timestamp(), nullable=False)
-    #is_done = db.Column(db.Boolean, server_default="false", nullable=False)
+    is_done = db.Column(db.Boolean, server_default="false", nullable=False)
 
-    def __init__(self, title, complexity, urgency, importance):
+    # foreign key - many tasks to one user
+    user_id = db.Column(db.Integer(), db.ForeignKey('users.id'))
+    user_relation = db.relationship('Users', backref="users")
+
+    def __init__(
+        self, title, description, due_datetime, complexity, urgency, importance,
+        user_id):
 
         self.title = title
+        self.description = description
+        self.due_datetime = due_datetime
         self.complexity = complexity
         self.urgency = urgency
         self.importance = importance
+        self.created_at = datetime.datetime.now()
+        self.user_id = user_id
 
 
 class TasksSchema(Schema):
 
-    not_blank = validate.Length(min=3, error='Field cannot be blank')
+    not_blank = validate.Length(min=3, error='Field cannot be blank or so short')
+
     id = fields.Integer(dump_only=True)
-    #user_id = fields.Nested(UsersSchema, only=['id', 'name'])
     title = fields.Email(validate=not_blank)
+    description = fields.String()
+    due_datetime = fields.DateTime(dump_only=True)
     complexity = fields.Integer()
     urgency = fields.Integer()
     importance = fields.Integer()
+    created_at = fields.DateTime(dump_only=True)
+    is_done = fields.Boolean()
+    user_id = fields.Integer()
+
 
      #self links
     def get_top_level_links(self, data, many):
--- app/users/models.py
+++ app/users/models.py
@@ -1,31 +1,24 @@
 from marshmallow_jsonapi import Schema, fields
 from marshmallow import validate
-from app.db import db
+from app.basemodels import db, CRUD
 
-class CRUD():
-
-    def add(self, resource):
-        db.session.add(resource)
-        return db.session.commit()
-
-    def update(self):
-        return db.session.commit()
-
-    def delete(self, resource):
-        db.session.delete(resource)
-        return db.session.commit()
 
 class Users(db.Model, CRUD):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(250), unique=True, nullable=False)
+    password = db.Column(db.String(250), nullable=False)
     name = db.Column(db.String(250), nullable=False)
-    creation_time = db.Column(db.TIMESTAMP, server_default=db.func.current_timestamp(), nullable=False)
     is_active = db.Column(db.Boolean, server_default="false", nullable=False)
+    created_at = db.Column(
+        db.TIMESTAMP, server_default=db.func.current_timestamp(), nullable=False)
+    modified_at = db.Column(
+        db.TIMESTAMP, server_default=db.func.current_timestamp(), nullable=False)
 
-    def __init__(self,  email,  name, is_active):
+    def __init__(self, email, password, name, is_active):
 
         self.email = email
+        self.password = password
         self.name = name
         self.is_active = is_active
 
@@ -33,10 +26,14 @@ class Users(db.Model, CRUD):
 class UsersSchema(Schema):
 
     not_blank = validate.Length(min=1, error='Field cannot be blank')
+
     id = fields.Integer(dump_only=True)
     email = fields.Email(validate=not_blank)
+    password = fields.Email(validate=not_blank)
     name = fields.String(validate=not_blank)
     is_active = fields.Boolean()
+    creation_time = fields.DateTime(dump_only=True)
+    modification_time = fields.DateTime(dump_only=True)
 
 
      #self links
